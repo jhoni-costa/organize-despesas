51773a804c21f1d6802d566ca7c244cf31725fdb
jhoni-costa <jhoni_costa@outlook.com>
2018-10-10 18:42:21 -0300

    Slide de Cadastro e Login

---
+++ app/src/main/java/br/com/jhonicosta/organizze/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package br.com.jhonicosta.organizze.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.jhonicosta.organizze.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/br/com/jhonicosta/organizze/activity/MainActivity.java
+++ app/src/main/java/br/com/jhonicosta/organizze/activity/MainActivity.java
@@ -1,16 +1,21 @@
-package br.com.jhonicosta.organizze;
+package br.com.jhonicosta.organizze.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+import br.com.jhonicosta.organizze.R;
 
 public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
 
         setButtonBackVisible(false);
         setButtonNextVisible(false);
@@ -34,5 +39,21 @@ public class MainActivity extends IntroActivity {
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_4)
                 .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
+                .build());
+
+
+    }
+
+    public void btnCadastrar(View view) {
+        startActivity(new Intent(getApplicationContext(), CadastroActivity.class));
+    }
+
+    public void txtEntrar(View view) {
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
 }
