243e6b34991cf573d5a40baaecb12bc4b857b590
jhoni-costa <jhoni_costa@outlook.com>
2018-10-15 16:29:22 -0300

    Cadastro de usuario no Firebase

--- app/src/main/java/br/com/jhonicosta/organizze/activity/CadastroActivity.java
+++ app/src/main/java/br/com/jhonicosta/organizze/activity/CadastroActivity.java
@@ -1,15 +1,82 @@
 package br.com.jhonicosta.organizze.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.jhonicosta.organizze.R;
+import br.com.jhonicosta.organizze.config.ConfigFirebase;
+import br.com.jhonicosta.organizze.model.Usuario;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText campoNome, campoEmail, campoSenha;
+    private Button botaoCadatrar;
+
+    private FirebaseAuth autenticacao;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        campoNome = findViewById(R.id.editNome);
+        campoEmail = findViewById(R.id.editEmail);
+        campoSenha = findViewById(R.id.editSenha);
+
+        botaoCadatrar = findViewById(R.id.buttonCadastrar);
+
+        botaoCadatrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txtNome = campoNome.getText().toString();
+                String txtEmail = campoEmail.getText().toString();
+                String txtSenha = campoSenha.getText().toString();
+
+                if (!txtNome.isEmpty()) {
+                    if (!txtEmail.isEmpty()) {
+                        if (!txtSenha.isEmpty()) {
+                            usuario = new Usuario(txtNome, txtEmail, txtSenha);
+                            cadastrarUsuario();
+                        } else {
+                            Toast.makeText(CadastroActivity.this, "Preencha a senha!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(CadastroActivity.this, "Preencha o e-mail!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(CadastroActivity.this, "Preencha o nome!", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+    }
+
+    public void cadastrarUsuario() {
+
+        autenticacao = ConfigFirebase.getFirebaseAuth();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(CadastroActivity.this, "Usuário cadatrado com sucesso!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CadastroActivity.this, "Erro ao cadatrar usuário!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/br/com/jhonicosta/organizze/config/ConfigFirebase.java
@@ -0,0 +1,15 @@
+package br.com.jhonicosta.organizze.config;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ConfigFirebase {
+
+    private static FirebaseAuth auth;
+
+    public static FirebaseAuth getFirebaseAuth() {
+        if (auth == null) {
+            auth = FirebaseAuth.getInstance();
+        }
+        return auth;
+    }
+}
